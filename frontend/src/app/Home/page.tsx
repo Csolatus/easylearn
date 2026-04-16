018e4058858423afd31a74728efcb80c88d64433
Kakoto <vinhlam.le29@gmail.com>
2026-04-16 21:04:34 +0200

    fix(frontend): replace Navbar with Header in Home page

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/layout/Footer";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-[#0d0d1a] text-white">
-      <Navbar />
+      <Header />
       <HeroSection />
       <Footer />
     </div>
